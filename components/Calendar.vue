645e0bf4b9e4a3ab3398b59db8a27411743bba8e
Marshall <mzmarshall1278@gmail.com>
2020-08-18 20:35:27 +0100

    added task description modal

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -52,11 +52,53 @@
           @click:more="viewDay"
           @click:date="viewDay"
           :event-color="getEventColor"
+          @click:event="showEvent"
         ></v-calendar>
-        
-          <!-- @change="updateRange" -->
-          <!-- @click:event="showEvent" -->
           
+           <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          offset-x
+        >
+          <v-card
+            color="grey lighten-4"
+            min-width="350px"
+            flat
+          >
+            <v-toolbar
+              :color="selectedEvent.color"
+              dark
+            >
+              <!-- <v-btn icon>
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn> -->
+              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+              <!-- <v-spacer></v-spacer> -->
+              <!-- <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn> -->
+            </v-toolbar>
+            <v-card-text>
+              <!-- <span v-html="selectedEvent.details"></span> -->
+              <p class="black--text">Adashi WIthdrawal Turn for Position {{selectedEvent.position }}<br>
+                <strong>NGN {{selectedEvent.payment}}</strong>
+              </p>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                text
+                color="secondary"
+                @click="selectedOpen = false"
+              >
+                Cancel
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
       </v-sheet>
     </v-col>
   </v-row>
@@ -73,13 +115,14 @@
         week: 'Week',
         day: 'Day',
       },
-      events: [
-        // { "name": "Payment", "start": "2020-08-19", color: "red" },
-        // { "name": "Young Money", "payment": 47500, "start": "2020-08-19", "position": 1 }, { "name": "Young Money", "payment": 47500, "start": "2020-08-26", "position": 2 }
-      ],
+      events: [],
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false,
       categories: [
         {name : "Young Money", Ammount: 10000, payment: 47500, members: 10, interval: '7', start: '2020-08-12', finished: "false"},
         {name : "Cash Money", Ammount: 30000, payment: 143500, members: 5, interval: '10', start: '2020-08-20', finished: "false"},
+        {name : "Ultimate", Ammount: 2000, payment: 275000, members: 15, interval: '10', start: '2020-08-20', finished: "false"},
       ] 
     }),
     mounted () {
@@ -135,8 +178,23 @@
       next () {
         this.$refs.calendar.next()
       },
-      
-    },
+       showEvent ({ nativeEvent, event }) {
+        const open = () => {
+          this.selectedEvent = event
+          this.selectedElement = nativeEvent.target
+          setTimeout(() => this.selectedOpen = true, 10)
+        }
+
+        if (this.selectedOpen) {
+          this.selectedOpen = false
+          setTimeout(open, 10)
+        } else {
+          open()
+        }
+
+        nativeEvent.stopPropagation()
+      } 
+    }
   }
   // Date and time format : 2020-08-19 00:30
 </script>
