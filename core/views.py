a0f774d0031cdf8424887772e080ea558178f9fa
nicksonlangat <nicksonlangat95@gmail.com>
2020-10-27 11:06:52 +0300

    fixes

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from . import forms
-from . import models
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
@@ -45,14 +44,14 @@ def money_transfer(request):
         if form.is_valid():
             form.save()
         
-            curr_user = models.MoneyTransfer.objects.get(enter_your_user_name=request.user)
+            curr_user = MoneyTransfer.objects.get(enter_your_user_name=request.user)
             dest_user_acc_num = curr_user.enter_the_destination_account_number
 
             temp = curr_user # NOTE: Delete this instance once money transfer is done
             
-            dest_user = models.Status.objects.get(account_number=dest_user_acc_num) # FIELD 1
+            dest_user = Status.objects.get(account_number=dest_user_acc_num) # FIELD 1
             transfer_amount = curr_user.enter_the_amount_to_be_transferred_in_USD # FIELD 2
-            curr_user = models.Status.objects.get(user_name=request.user) # FIELD 3
+            curr_user = Status.objects.get(user_name=request.user) # FIELD 3
 
             # Now transfer the money!
             curr_user.balance = curr_user.balance - transfer_amount
