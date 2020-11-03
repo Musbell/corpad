8913ede12f8126a6742e1a9121134904653e4fde
nicksonlangat <nicksonlangat95@gmail.com>
2020-11-03 09:36:12 +0300

    adds

--- api/views.py
+++ api/views.py
@@ -5,6 +5,14 @@ from .serializers import *
 
 # Create your views here.
 
+class PositionList(generics.ListCreateAPIView):
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializer
+
+class PositionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializer
+
 class JoinList(generics.ListCreateAPIView):
     queryset = Join.objects.all()
     serializer_class = JoinSerializer
